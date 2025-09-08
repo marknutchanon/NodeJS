77d29cc18d6dfd80b3d9c1c17ae214d1817e4cef
Nutchanon U-songtham <56406245+marknutchanon@users.noreply.github.com>
2025-09-09 04:02:08 +0700

    Add all files
    Start Project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    debug(`Example app listening on port ` + chalk.red(`${port}`));
+});
