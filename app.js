747880fb2e286054c6e4542264d0cbdd52b8e979
Nutchanon U-songtham <56406245+marknutchanon@users.noreply.github.com>
2025-09-09 08:15:11 +0700

    view index

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const chalk = require('chalk');
-const debug = require('debug')('app');
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express")
+const chalk = require("chalk")
+const debug = require("debug")("app")
+const morgan = require("morgan")
+const path = require("path")
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("combined"))
+app.use(express.static(path.join(__dirname, "/public/")))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.set("views", "./src/views")
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index", { username: "John", customers: ["Alice", "Bob", "Charlie"] })
+})
 
 app.listen(port, () => {
-    debug(`Example app listening on port ` + chalk.red(`${port}`));
-});
+    debug(`Server is running on ` + chalk.green(`http://localhost:${port}`))
+})
